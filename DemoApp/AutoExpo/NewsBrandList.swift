08d8882d1f9b78f7a7f9574e1dfb9f143bde1722
NarendraVerma <narendra.sr27@gmail.com>
2020-07-11 16:11:26 +0530

    Auto Expo

---
+++ DemoApp/AutoExpo/NewsBrandList.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  DemoApp
+//
+//  Created by Narendra on 11/07/20.
+//  Copyright © 2020 DemoApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewsBrandList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsBrandList_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsBrandList()
+    }
+}
